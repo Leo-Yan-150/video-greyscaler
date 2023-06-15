7bdde8074774853ac3bb1f4e3eec82131c67a128
Leo <76232822+Leo-Yan-150@users.noreply.github.com>
2023-06-14 18:12:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import cv2
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, redirect, url_for, send_file, app
 from werkzeug.utils import secure_filename
 
 
